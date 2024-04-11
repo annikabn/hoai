6ae281a0d84a1286b975006549367d7b9f16c7e4
Annika Brandt Nielsen <annika@AirtilhndeKatja.localdomain>
2024-04-11 09:55:42 +0200

    git commit -m "flex boxes"

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,42 @@ import viteLogo from "/vite.svg";
 
 function App() {
   return (
-    <div>
+    <div className="fullscreen">
       <header>
         <h1>Flex</h1>
       </header>
-
-      <div>
-        <h3 className="age" id="birthday">
-          My Birthday:
-        </h3>
-        <input type="date" className="selectors" id="calender"></input>
-      </div>
-      <div>
-        <h3 className="age" id="age">
-          My Age in:
-        </h3>
-        <select className="selectors" id="dropdown">
-          <option value="years">Years</option>
-          <option value="months">Months</option>
-          <option value="weeks">Weeks</option>
-          <option value="days">Days</option>
-          <option value="hours">Hours</option>
-          <option value="minutes">Minutes</option>
-          <option value="seconds">Seconds</option>
-        </select>
-      </div>
-      <div>
-        <h3 className="results" id="im">
-          I am
-        </h3>
-        <h2 className="results" id="result">
-          ___________
-        </h2>
-        <h3 className="results" id="old">
-          old
-        </h3>
+      <div className="maincontainer">
+        <div className="children">
+          <h3 className="age" id="birthday">
+            My Birthday:
+          </h3>
+          <input type="date" className="selectors" id="calender"></input>
+        </div>
+        <div className="children">
+          <h3 className="age" id="age">
+            My Age in:
+          </h3>
+          <select className="selectors" id="dropdown">
+            <option value="years">Years</option>
+            <option value="months">Months</option>
+            <option value="weeks">Weeks</option>
+            <option value="days">Days</option>
+            <option value="hours">Hours</option>
+            <option value="minutes">Minutes</option>
+            <option value="seconds">Seconds</option>
+          </select>
+        </div>
+        <div className="children">
+          <h3 className="results" id="im">
+            I am
+          </h3>
+          <h2 className="results" id="result">
+            ___________
+          </h2>
+          <h3 className="results" id="old">
+            old
+          </h3>
+        </div>
       </div>
     </div>
   );
